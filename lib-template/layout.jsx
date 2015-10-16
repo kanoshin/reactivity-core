a27d3e8813e30e1648d6a84eb4825ca973fdf3fb
Kirill Anoshin <kanoshin@servicetitan.com>
2015-10-17 00:49:12 +0500

    leftnav

--- lib-template/layout.jsx
+++ lib-template/layout.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
-import { AppBar } from 'material-ui'
+import { AppBar, LeftNav } from 'material-ui'
+import Radium from 'radium'
+
+let menuItems = [
+    { route: 'dashboard', text: 'Dashboard' },
+    { route: 'charts', text: 'Charts' },
+    { route: 'forms', text: 'Forms' }
+  ];
 
 class Layout extends React.Component {
+	constructor() {
+		super();
+		this.styles = {
+			navBar: {
+				marginTop: '65px',
+				zIndex: 0
+			}
+		};
+	}
+
 	render() {
 		return (
 		<div>
 			<AppBar title='Reactivity' />
+			<LeftNav ref="leftNav"
+				docked={true}
+				menuItems={menuItems}
+				style={this.styles.navBar} />
 			{this.props.children}
 		</div>);
 	}
+	
+	componentDidMount() {
+		//this.refs.leftNav.open();
+	}
 }
+
+Layout = Radium(Layout);
 
 export default Layout
