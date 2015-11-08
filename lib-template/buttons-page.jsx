f6305fbe1a544803c444755ba32f2a6a0372e588
Kirill Anoshin <kanoshin@servicetitan.com>
2015-11-09 01:54:20 +0500

    choose image button

--- lib-template/buttons-page.jsx
+++ lib-template/buttons-page.jsx
@@ -10,6 +10,18 @@ let styles = {
 		verticalAlign: 'top',
     	padding: '5px 0px 0px 16px',
 		float: 'left'
+	},
+	imageInput: {
+		cursor: 'pointer',
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        right: '0',
+        left: '0',
+        width: '100%',
+        opacity: '0',
+		overflow: 'hidden',
+    	paddingLeft: '100%'
 	}
 };
 
@@ -40,7 +52,7 @@ class FormsPage extends React.Component {
 							</Cell>
 							<Cell size='1/3'>
 								<FlatButton primary={true} label="Choose an Image">
-									<input type="file" id="imageButton" style={{display: 'none'}}></input>
+									<input type="file" id="imageButton" style={styles.imageInput}></input>
 								</FlatButton>
 							</Cell>
 							<Cell size='1/3'>
