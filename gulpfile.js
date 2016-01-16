1a18d39f7a396878d9ee760fe27295a6a933b66c
Kirill Anoshin <kanoshin@servicetitan.com>
2016-01-16 16:53:38 +0500

    additional build libraries

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('dev-build', function() {
             return builder.loadConfig('./lib-template/custom-config.js');
         })
         .then(function() {
-            return builder.bundle('material-ui + react + react-router + material-ui/lib/svg-icons', './build/bundle.js', {
+            return builder.bundle('material-ui + react + react-dom + react-router + material-ui/lib/svg-icons + radium + core-js + react-flexr', './build/bundle.js', {
                 sourceMaps: true
             });
         });
